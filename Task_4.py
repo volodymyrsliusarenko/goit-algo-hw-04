35d312ca39c98268bdf220829d3f7c26c829d7c3
Volodymyr Sliusarenko <volodymyr.slusarenko@gmail.com>
2024-03-16 15:13:27 +0200

    finish 4 task

---
+++ Task_4.py
@@ -0,0 +1,60 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        raise ValueError("Contact alredy exists, please try new name, or 'change' command to update")
+    contacts[name] = phone
+    return "Contact added."
+
+
+def change_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        name, phone = args
+    contacts[name] = phone
+    return "Contact updated successfully"
+
+def show_contact(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return contacts[name]
+    else:
+        return "No such contact"
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            print(add_contact(args, contacts))
+
+        elif command == "change":
+            print(change_contact(args, contacts))
+
+        elif command == "phone":
+            print(show_contact(args, contacts))
+
+        elif command == "all":
+            print(contacts)
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
